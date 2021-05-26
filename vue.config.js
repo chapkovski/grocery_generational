3e0099cdcc3001ffd080b8fd6f9a692e3abf465c
Francisco Kahil <codexico@gmail.com>
2021-05-26 22:35:19 +0300

    vueconfig upd

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
 module.exports = {
     lintOnSave: false,
+    publicPath: process.env.NODE_ENV === 'production'
+        ? ''
+        : '/',
+    "transpileDependencies": [
+        "vuetify"
+    ],
 
-    transpileDependencies: [
-      'vuetify'
-    ]
 }
